4e4f7f6a5c374b2bc154a36c53ae3e947c3b2990
tobiasjongen <98836219+tobiasjongen@users.noreply.github.com>
2022-06-14 20:39:46 +0200

    add with

--- Uebung3/exercise02/sub_a.sql
+++ Uebung3/exercise02/sub_a.sql
@@ -1,4 +1,16 @@
-SELECT * FROM
+WITH ActionFilm AS (
+    SELECT genre, movie_id FROM genre WHERE genre='Action'
+), TSchau AS (
+    SELECT movie_id, name FROM actor WHERE name LIKE 'T%'
+), ActionSchau AS (
+    SELECT * FROM TSchau t LEFT OUTER JOIN ActionFilm a ON t.movie_id = a.movie_id
+)
+
+SELECT DISTINCT name FROM ActionSchau WHERE genre IS NOT NULL ORDER BY name ASC;
+
+----
+
+SELECT name FROM
 (
     (SELECT name, movie_id FROM actor WHERE name LIKE 'T%') as actor_T 
 JOIN 
